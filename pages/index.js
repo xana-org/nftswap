3f6fc22297905a782017d742e481f81fc140e8a9
panda6853 <jackielee6853@gmail.com>
2021-03-24 13:41:28 -0400

    added erc20 token selection modal

--- pages/index.js
+++ pages/index.js
@@ -27,6 +27,7 @@ import {
   ArrowRightIcon,
   CloseIcon
 } from "@chakra-ui/icons";
+import InfiniteScroll                from "react-infinite-scroller";
 import { COINGECKO_URL, LEFT_TOKEN } from "../constants/const";
 import { formatNumber }         from "../lib/helper";
 import { getBalance }           from "../contracts/erc20";
@@ -39,21 +40,25 @@ import Axios from "axios";
 const Home = () => {
   const [type_left, setTypeLeft] = useState("l_erc20");
   const [tokenList, setTokenList] = useState([]);
+  const [filteredTokenList, setFilteredTokenList] = useState([]);
   const [lefToken, setLeftToken] = useState(LEFT_TOKEN);
   const [balance, setBalance] = useState(-1);
   const [isOpenERC20, setIsOpenERC20] = useState(false);
+  const [tokenShowCount, setTokenShowCount] = useState(30);
+  const [filterText, setFilterText] = useState("");
   const wallet = useWallet();
 
   useEffect(() => {
     Axios.get(COINGECKO_URL).then(res => {
       setTokenList(res.data.tokens);
+      setFilteredTokenList(res.data.tokens);
     });
   }, [])
   
-  const getTokenBalance = async () => {
+  const getTokenBalance = async (token) => {
     if (type_left !== "l_erc20") return;
     const provider = new ethers.providers.Web3Provider(wallet.ethereum);
-    const { address, decimals } = lefToken;
+    const { address, decimals } = token;
     const walletAddress = getWalletAddress(wallet);
     if (!address) return;
     let balance = await getBalance(address, walletAddress, provider);
@@ -66,19 +71,22 @@ const Home = () => {
 
   useEffect(() => {
     if (wallet && wallet.ethereum) {
-      getTokenBalance();
+      getTokenBalance(lefToken);
     }
   }, [wallet])
+
   const renderLeftToken = () => {
     return (
       <Box mt="1rem" ml="auto" w="100%">
         <Flex flexDirection="row" justifyContent="flex-end">
           <Flex flexDirection="row" cursor="pointer" userSelect="none">
-            <Box w="3.4rem" h="3.4rem" m="auto .5rem" borderRadius="100%" bg="blue.800">
+            <Box w="3.4rem" h="3.4rem" m="auto .5rem" borderRadius="100%" bg="blue.800" onClick={() => {setIsOpenERC20(true)}}>
               <Image src={lefToken.logoURI} w="2rem" h="2rem" m="0.7rem"/>
             </Box>
-            <Text fontSize="24px" fontWeight="bold" m="auto 0">{lefToken.symbol}</Text>
-            <ChevronDownIcon m="auto 0.4rem"/>
+            <Flex flexDirection="row" onClick={() => {setIsOpenERC20(true)}}>
+              <Text fontSize="24px" fontWeight="bold" m="auto 0">{lefToken.symbol}</Text>
+              <ChevronDownIcon m="auto 0.4rem"/>
+            </Flex>
             <NumberInput w="100px" m="auto 0">
               <NumberInputField/>
             </NumberInput>
@@ -93,6 +101,47 @@ const Home = () => {
         </Flex>
       </Box>
     )
+  }
+
+  const onOpenTokenLink = async (e, addr) => {
+    e.preventDefault();
+    e.stopPropagation();
+		const ethersProvider = new ethers.providers.Web3Provider(wallet.ethereum);
+    const network = await ethersProvider._networkPromise;
+    if (network.chainId === 1) {
+      window.open("https://etherscan.io/address/" + addr);
+    }
+  }
+
+  const loadMoreToken = () => {
+    setTokenShowCount(tokenShowCount + 30 > filteredTokenList.length ? filteredTokenList.length : tokenShowCount + 30);
+  }
+  
+  const onFilterTextChange = (e) => {
+    setFilterText(e.target.value);
+    if (e.target.value) {
+      const fText = e.target.value.toLowerCase();
+      const fList = tokenList.filter((item) => {
+        return item.address.toLowerCase().indexOf(fText) >= 0 ||
+          item.name.toLowerCase().indexOf(fText) >= 0 || 
+          item.symbol.toLowerCase().indexOf(fText) >= 0
+      });
+      setFilteredTokenList(fList);
+    } else setFilteredTokenList(tokenList);
+  }
+
+  const onErc20TokenSelection = (token) => {
+    setLeftToken({
+      type: 0,
+      name: token.name,
+      symbol: token.symbol,
+      address: token.address,
+      logoURI: token.logoURI,
+      decimals: token.decimals
+    });
+    setBalance(-1);
+    setIsOpenERC20(false);
+    getTokenBalance(token);
   }
 
   return (
@@ -143,7 +192,6 @@ const Home = () => {
           </Flex>
           <Box w="40%">
             <Text
-              textAlign="center"
               fontSize="26px"
               fontWeight="bold"
             >
@@ -164,7 +212,7 @@ const Home = () => {
           </Box>
         </Flex>
       </Box>
-      <Modal size="md" isOpen={true}>
+      <Modal size="md" isOpen={isOpenERC20} onClose={() => setIsOpenERC20(false)}>
         <ModalOverlay/>
         <ModalContent borderRadius="5px" bg="white" p="2rem 1rem">
           <IconButton
@@ -190,24 +238,48 @@ const Home = () => {
               pointerEvents="none"
               children={<SearchIcon color="gray.300" />}
             />
-            <Input placeholder="Search token by name, symbol or address" />
+            <Input
+              placeholder="Search token by name, symbol or address"
+              value={filterText}
+              onChange={onFilterTextChange}
+            />
           </InputGroup>
           <Text fontSize="24px" fontWeight="bold" mt="2rem" mb="1rem">
             Trusted By
           </Text>
-          <Flex flexDirection="column" h="300px" overflow="auto">
-            {tokenList.map((item, index) => {
-              return (
-                <Flex bg="gray.100" p="1rem" mb="10px" key={index} flexDirection="row">
-                  <Image src={item.logoURI} w="1.6rem" h="1.6rem" m="auto 1rem auto 0"/>
-                  <Flex flexDirection="column">
-                    <Text fontSize="18px" fontWeight="500">{item.name}</Text>
-                    <Text fontSize="12px" color="gray.600">{item.symbol}</Text>
+          <Box h="300px" overflow="auto">
+            <InfiniteScroll
+              hasMore={true}
+              loadMore={loadMoreToken}
+              pageStart={0}
+              initialLoad={false}
+              useWindow={false}
+            >
+              {filteredTokenList.slice(0, tokenShowCount).map((item, index) => {
+                return (
+                  <Flex 
+                    bg="gray.100"
+                    p="0.6rem 1rem"
+                    mb="10px"
+                    key={index}
+                    flexDirection="row"
+                    borderRadius="8px"
+                    cursor="pointer"
+                    onClick={() => onErc20TokenSelection(item)}
+                  >
+                    <Image src={item.logoURI} w="1.6rem" h="1.6rem" m="auto 1rem auto 0"/>
+                    <Flex flexDirection="column">
+                      <Text fontSize="18px" fontWeight="500">{item.name}</Text>
+                      <Text fontSize="12px" color="gray.600">{item.symbol}</Text>
+                    </Flex>
+                    <Flex flexDirection="column" ml="auto" justifyContent="flex-end" cursor="pointer" onClick={(e) => onOpenTokenLink(e, item.address)}>
+                      <Text fontSize="12px" color="gray.600" m="auto 0" as="u">{shortenWalletAddress(item.address)}</Text>
+                    </Flex>
                   </Flex>
-                </Flex>
-              )
-            })}
-          </Flex>
+                )
+              })}
+            </InfiniteScroll>
+          </Box>
         </ModalContent>
       </Modal>
     </Box>
